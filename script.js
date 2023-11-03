1a86c31266f23effe5d05b49df72079f47e0fc65
tferguson1028 <tferguson1028@outlook.com>
2023-11-02 23:19:05 -0400

    Finished

---
+++ script.js
@@ -0,0 +1,28 @@
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.querySelector("nav#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
+menuLinks.forEach((link) => 
+{
+  let anchorEl = document.createElement("a");
+  anchorEl.href = link.href;
+  anchorEl.textContent = link.text;
+  topMenuEl.appendChild(anchorEl);
+});
+
+
+
